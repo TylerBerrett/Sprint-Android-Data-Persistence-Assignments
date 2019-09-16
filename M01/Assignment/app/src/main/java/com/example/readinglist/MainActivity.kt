17d513b81c0ea2f5d60d6cd6009c47cee9888d2f
Tyler Berrett <btylerberrett@gmail.com>
2019-09-16 14:43:21 -0600

    add model

--- M01/Assignment/app/src/main/java/com/example/readinglist/MainActivity.kt
+++ M01/Assignment/app/src/main/java/com/example/readinglist/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.readinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.readinglist.model.Book
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun buildItemView(book: Book){
+
+        }
+
+
     }
 }
---
+++ M01/Assignment/app/src/main/java/com/example/readinglist/model/Book.kt
@@ -0,0 +1,36 @@
+package com.example.readinglist.model
+
+class Book{
+    var title: String = ""
+    var reasonToRead: String = ""
+    var hasBeenRead: Boolean = false
+    var id: String = ""
+
+    constructor(title: String, reasonToRead: String, hasBeenRead: Boolean, id: String){
+        this.title = title
+        this.reasonToRead = reasonToRead
+        this.hasBeenRead = hasBeenRead
+        this.id = id
+    }
+
+    constructor(itemToCsv: String){
+        val params = itemToCsv.split(",")
+        for (i in params.indices) {
+            val param = params[i]
+            when (i) {
+                0 -> this.title = param
+                1 -> this.reasonToRead = param
+                2 -> this.hasBeenRead = param.toBoolean()
+                3 -> this.id = param
+            }
+        }
+
+    }
+
+    fun getTheTitle(): String{
+        return title
+    }
+
+    fun toCsvString(book: Book): String = "${book.title},${book.reasonToRead},${book.hasBeenRead},${book.id}"
+
+}
